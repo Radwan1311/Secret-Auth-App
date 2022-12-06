3a773d88bc5398d8f07029727068bc8f1cc02560
Radwan1311 <radwan.1311.mr@gmail.com>
2022-12-06 23:42:07 +0200

    Adding the 10 round Hash auth

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
-const md5 = require('md5');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
+const saltRounds = 10;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -45,36 +46,45 @@ app.get('/register',function(req,res){
 
 
 app.post('/register',function(req,res){
-  const newUser = new User({
-    email: req.body.username ,
-    password :md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username ,
+      password :hash
+    });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render('secrets');
-    }
-  });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render('secrets');
+      }
+    });
+     });
 
 });
 
 app.post('/login' , function(req ,res){
   const username = req.body.username;
-  const password =md5(req.body.password);
+  const password =req.body.password;
 
 User.findOne({email:username}  , function(err , foundUser){
   if(err){
     console.log(err);
   }else{
     if(foundUser){
-      if(foundUser.password == password){
-        res.render('secrets')
-      }else{
-        console.log("wrong Password");
+
+      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+        if(result === true){
+            res.render('secrets')
+        }else{
+            console.log("wrong Password");
+        }
+      });
+
+
+
       }
-    }
+
   }
 });
 
